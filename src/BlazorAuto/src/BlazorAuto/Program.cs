09dda350ffdcf68e179b86975db4380df4e0a5f2
mkArtak <artak.services+github@outlook.com>
2024-04-21 17:56:38 -0700

    Added API Endpoints for job listings

--- src/BlazorAuto/src/BlazorAuto/Program.cs
+++ src/BlazorAuto/src/BlazorAuto/Program.cs
@@ -1,5 +1,7 @@
 using BlazorAuto.Client.Pages;
 using BlazorAuto.Components;
+using BlazorAuto.Shared;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -27,9 +29,37 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseAntiforgery();
 
+List<JobListing> allListings = new List<JobListing> {
+    CreateJobListing(0, "Software Engineer", "IT", "Develop and optimize code across platforms, ensuring robust and scalable applications."),
+    CreateJobListing(1, "Software Engineer", "IT", "Engineer software solutions from concept to deployment, focusing on performance and security."),
+    CreateJobListing(2, "Software Engineer", "IT", "Collaborate in agile teams to design, test, and iterate on enterprise-level software."),
+    CreateJobListing(3, "Senior Software Engineer", "IT", "Integrate systems and APIs, improve software architecture, and support deployment processes."),
+    CreateJobListing(4, "Senior Software Engineer", "IT", "Build front-end and back-end systems for real-time data processing and analytics."),
+    CreateJobListing(5, "Software Program Manager", "IT", "Oversee product lifecycle, define vision, and align teams to strategic goals."),
+    CreateJobListing(6, "Senior Software Program Manager", "IT", "Lead cross-functional teams to launch products that meet market and user needs."),
+    CreateJobListing(7, "Software Program Manager", "IT", "Analyze market trends to position products competitively and drive innovation."),
+    CreateJobListing(8, "Software Program Manager", "IT", "Manage product roadmaps, prioritize features, and ensure timely delivery."),
+    CreateJobListing(9, "Software Program Manager", "IT", "Coordinate with stakeholders to refine product offerings and improve user satisfaction."),
+};
+
+app.MapGet("/jobs", () => allListings);
+app.MapGet("/jobs/{key}", (string key) => allListings.Where(listing => listing.Title.Contains(key, StringComparison.InvariantCultureIgnoreCase)));
+app.MapPost("/jobs", (JobListing listing) => allListings.Add(listing));
+
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(BlazorAuto.Client._Imports).Assembly);
 
 app.Run();
+
+JobListing CreateJobListing(int id, string title, string category, string description)
+    => new JobListing
+    {
+        Id = id,
+        IsActive = true,
+        DatePosted = DateTimeOffset.UtcNow,
+        Category = category,
+        Title = title,
+        Description = description
+    };
